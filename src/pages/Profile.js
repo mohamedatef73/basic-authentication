7478605b50e411e588cec6ee75a8a99b5448a6ae
mohamedatef73 <matef4202@gmail.com>
2020-08-09 21:43:04 +0200

    Update the Profile Page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,20 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ProfileForm from '../components/formpages/ProfileForm'
 import { useSelector } from 'react-redux'
 
-const ProfilePage =() =>{
+
+const ProfilePage = () => {
     const student = useSelector(state => state.Authenticate.students)
 
+    const [data, setData] = useState([]);
 
+    useEffect(() => {
+        // fetch("https://jsonplaceholder.typicode.com/photos")
+        //     .then(response => response.json())
+        //     .then(data => setData(data));
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            body: JSON.stringify({
+                title: 'The Result is',
+                body: 'Accepted',
+                id:1
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+            .then(response => response.json())
+            .then(json => setData(json));
 
-    return(
+    });
+
+    return (
         <div className='profile col-12'>
-            <div  className='home text-center'>
-                <br/><br/><br/>
-            <h1> welcome <br/>
-            <span className='name'> {student.name}</span></h1>
-            <ProfileForm />
-            </div>
+            <div className='home text-center'>
+                <br /><br /><br />
+                <h1> welcome <br />
+                    <span className='name'> {student.name}</span></h1>
+                <ProfileForm />
+            </div><p />
+            <ul>
+                {data.title} <br/><h1>{data.id}</h1> 
+                {/* {data.map(el => (
+                    <p>{el.title}</p>
+                ))} */}
+            </ul>
         </div>
     )
 }
